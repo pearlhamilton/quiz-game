59050beff725facab0469a4c10449c9e07dbac01
Pearl-web <56562067+Pearl-web@users.noreply.github.com>
2021-02-15 19:59:25 +0000

    save high scores in local storage

--- end.js
+++ end.js
@@ -3,6 +3,12 @@ const saveScoreButton = document.getElementById('saveScoreButton')
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('finalScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+console.log(highScores)
+
+
+
 finalScore.innerText =  mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,4 +18,20 @@ username.addEventListener('keyup', () => {
 
 
 saveHighScore = (e) => {
-    e.preventDefault();}
+    e.preventDefault();
+
+    const score ={
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score)
+    console.log(highScores)
+    highScores.sort((a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
+
+    console.log(score)
+
+}
