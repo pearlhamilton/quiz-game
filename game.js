c7dfafc4a44f6ebdd031c9b41cd9801e1e40f30b
Pearl-web <56562067+Pearl-web@users.noreply.github.com>
2021-02-16 17:15:03 +0000

    load questions from local json file

--- game.js
+++ game.js
@@ -13,33 +13,22 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-]; 
+let questions = []; 
+
+fetch("questions.json")
+    .then( res => {
+    console.log(res);
+    return res.json()
+})
+    .then(loadedQuestions => {
+        console.log(loadedQuestions);
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err =>{
+        console.error(err);
+    })
+    //catch is put there if something goes wrong with fetching from JSON
 
 
 //Constants
@@ -113,4 +102,3 @@ getNewQuestion = () => {
 
 
 
-startGame();
